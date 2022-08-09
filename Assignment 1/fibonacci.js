13da262a6d443abbf7c8b0942ba80b62a06378d9
tweinert <tait.weinert@outlook.com>
2022-08-09 18:13:14 +0930

    added iterative fibonacci

--- Assignment 1/fibonacci.js
+++ Assignment 1/fibonacci.js
@@ -1,3 +1,25 @@
-function fibs() {
+function fibs(n) {
+    let arr = [];
     
+    if (n >= 1) {
+        arr.push(0);
+    }
+
+    if (n >= 2) {
+        arr.push(1);
+    }
+
+    if (n >= 3) {
+        for (let i = 2; i < n; i++) {
+            value = arr[i - 1] + arr[i - 2];
+            arr.push(value);
+        }
+    }
+
+    return arr;
 }
+
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(8));
+console.log(fibs(13));
